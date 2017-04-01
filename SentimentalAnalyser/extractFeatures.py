4d8c8694f3281db8e70b9767275f9b0041b2482f
Jithin Roy <jithinroy@jithinmbp.mobile.asu.edu>
2017-04-01 16:20:12 -0700

    Completed Naive Bayes

--- SentimentalAnalyser/extractFeatures.py
+++ SentimentalAnalyser/extractFeatures.py
@@ -1,6 +1,20 @@
 from nltk.tokenize import word_tokenize
 import nltk
+from nltk.corpus import stopwords
+from random import shuffle
 
+"""
+Extracts features from the given file containing sentences and tags.
+
+getTokenizedData returns a list of training data. Each item in the list contains
+the list of words in that sentence and the tag.
+[
+	[['Item', 'Does', 'Not', 'Match', 'Picture', '.'], '0'],
+	[['Great', 'brunch', 'spot', '.'], '1'],
+	 ....
+
+]
+"""
 class ExtractFeatures:
 
 
@@ -8,9 +22,31 @@ class ExtractFeatures:
 		self.filePath = filePath
 
 	def getTokenizedData(self):
+		
 		# Read the file
+		print ''
+		print ("Extracting training data ")
+		stops = set(stopwords.words("english"))
+		result = []
 		for line in open(self.filePath):
-			word, tag = line.split('\t')
-			print tag
 			
-		# Tokenize the data
+			sentence, tag = line.split('\t')
+			tokenizedlist = word_tokenize(sentence)
+
+			# Uncomment the below line to use stop words.
+			# tokenizedlist = [word for word in tokenizedlist if word not in stops]
+			item = [tokenizedlist, tag.rstrip()]
+			result.append(item)
+
+		print ("Extracted tokenized words from training data - " + self.filePath)
+		shuffle(result)
+		
+		finalData = {}
+		count = len(result)
+		percentageOfTrainingData = 0.8
+		trainDataIndex = int(count * percentageOfTrainingData)
+
+		finalData['train'] = result[:trainDataIndex]
+		finalData['test'] = result[trainDataIndex:]
+		return finalData
+			
--- SentimentalAnalyser/sentimAnalyser.py
+++ SentimentalAnalyser/sentimAnalyser.py
@@ -1,17 +1,31 @@
 
 
-from nltk.tokenize import word_tokenize
-import nltk
 from extractFeatures import ExtractFeatures
+import nltk.classify.util
+from nltk.tokenize import word_tokenize
+from nltk.classify import NaiveBayesClassifier
 
+"""
+Class that generate the sentiment of the given sentence.
+
+Initializing the class will train the classifier with the 
+training data.
+
+Then you can use the method getSEntiments(message) to get the sentiment 
+value.
+
+"""
 class SentimentalAnalyzer:
 
 	def __init__(self):
 		self.valenceDict = {}
+		self.classifier = None
 		self.initializeValenceDict()
 
-	# Uses valence data from http://www2.imm.dtu.dk/pubdb/views/publication_details.php?id=6010
-	# The dataset contains words marked in range [-5,5] 
+	'''
+	Uses valence data from http://www2.imm.dtu.dk/pubdb/views/publication_details.php?id=6010
+	The dataset contains words marked in range [-5,5] 
+	'''
 	def initializeValenceDict(self):
 		for line in open('./data/AFINN/AFINN-111.txt'):
 			word, valenceValue = line.split('\t')
@@ -19,15 +33,37 @@ class SentimentalAnalyzer:
 		self.trainNaiveBayes()
 		
 
+	'''
+		Train a naivebayes classifer with the training data.
+	'''
 	def trainNaiveBayes(self):
-		# Get data from the 
+		
+		# Get data from the extracter
 		extracter = ExtractFeatures('./data/reviewsData.txt')
-		extracter.getTokenizedData()
+		tokenizedData = extracter.getTokenizedData()
+
+		trainingData = tokenizedData['train']
+		testData = tokenizedData['test']
+
+		print ''
+		print 'Training data size = ', len(trainingData)
+		print 'Test data size = ', len(testData)
+		print ''
+
+		modifiedTrainingData = [(self.word_feats(item[0]), item[1]) for item in trainingData]
+		modifiedTestData = [(self.word_feats(item[0]), item[1]) for item in testData]
+
+		self.classifier = NaiveBayesClassifier.train(modifiedTrainingData)
+		print 'accuracy:', nltk.classify.util.accuracy(self.classifier, modifiedTestData)
+
+	def word_feats(self, words):
+		return dict([(word, True) for word in words])
+
 
 	# Return 1 if happy 0 if sad
 	def getSentiments(self, message):
 		tokenizedwords = word_tokenize(message)
-		return 1
+		return self.classifier.classify(self.word_feats(tokenizedwords))
 
 obj = SentimentalAnalyzer() 
-obj.getSentiments('hello there i am here')
+print obj.getSentiments('I just find myself happy with the simple things. Appreciating the blessings God gave me.')
